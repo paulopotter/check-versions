5be01efbbd468f5840bb0afe3c49ec09fb793e54
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2016-10-16 19:49:13 -0200

    Create initial flask project

---
+++ run.py
@@ -0,0 +1,27 @@
+import os
+
+import yaml
+from flask import Flask, render_template  # NOQA
+from werkzeug.contrib.cache import SimpleCache
+
+app = Flask(__name__)
+cache = SimpleCache()
+
+
+def get_settings():
+    with open('settings.yaml', 'r') as file:
+        settings = yaml.load(file)
+
+    return settings
+
+
+@app.route("/")
+def index():
+    settings = cache.get("settings")
+    return render_template('index.html', project_name=settings['PROJECT']['NAME'])
+
+
+if __name__ == "__main__":
+    PORT = os.getenv('PORT', 5001)
+    cache.set("settings", get_settings())
+    app.run(port=int(PORT), host='0.0.0.0', debug=True)
