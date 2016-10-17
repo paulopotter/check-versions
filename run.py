c647b4540eff7a028ee22fc750f5996904df3265
Paulo Oliveira <paulo.oliveira@corp.globo.com>
2016-10-17 11:25:30 -0200

    put correct link on index

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import os
 import yaml
 from flask import Flask, render_template  # NOQA
 from werkzeug.contrib.cache import SimpleCache
+from slugify import slugify
 
 app = Flask(__name__)
 cache = SimpleCache()
@@ -18,7 +19,9 @@ def get_settings():
 @app.route("/")
 def index():
     settings = cache.get("settings")
-    return render_template('index.html', project_name=settings['PROJECT']['NAME'])
+    project_name = str(settings['PROJECT']['NAME'])
+    project_uri = slugify(project_name, to_lower=True)
+    return render_template('index.html', project_name=project_name, project_uri=project_uri)
 
 
 @app.route("/project")
